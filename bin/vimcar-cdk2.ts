2b038ed46c8a76f9e2252ce8bebdf84ccbc3be7b
Einstein <nwizuchibuzo@gmail.com>
2022-08-02 00:11:41 +0100

    added dashboard stack to entrypoint

--- bin/vimcar-cdk2.ts
+++ bin/vimcar-cdk2.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { VimcarCdk2Stack } from '../lib/vimcar-cdk2-stack';
+import {CdkVimcar2LambdaDashboardStack} from '../lib/vimcar-cdk2-lamba-dashboard';
 
 const app = new cdk.App();
-new VimcarCdk2Stack(app, 'VimcarCdk2Stack');
+
+
+const vimmcar2LambdaDashboardStack = new CdkVimcar2LambdaDashboardStack(app, 'VimcarLamdaDashboardStack',{
+    dashboardName: "VimcarLambdaDashboard"
+})
+const vimcarCdk2Stack = new VimcarCdk2Stack(app, 'VimcarCdk2Stack');
